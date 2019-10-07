3b54c4d73d9fa1e1577aeb7d91166f467e78d86c
Fernanda Andrade <fandrade@stackbuilders.com>
2019-10-07 01:25:46 -0500

    Add CI

--- index.test-d.ts
+++ index.test-d.ts
@@ -3,7 +3,8 @@ import * as myApi from '.';
 
 // getUser
 expectType<myApi.User>(myApi.getUser(1));
-expectError<myApi.User[]>(myApi.getUser(2));
+expectType<myApi.User>(myApi.getUser(100));
+expectError<myApi.User[]>(myApi.getUser(100));
 
 // getUsers
 expectType<myApi.User[]>(myApi.getUsers());
